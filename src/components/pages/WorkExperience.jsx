0f53ee6fa8d53a1f834c33e6d7d48f239ccb6f3d
Nguyen Hoang Phuc <phucnguyen2716@gmail.com>
2025-07-24 17:17:29 +0700

    Them

--- src/components/pages/WorkExperience.jsx
+++ src/components/pages/WorkExperience.jsx
@@ -4,9 +4,9 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-
 import { experiences } from "../../constants/experience";
 import { skills } from "../../constants/skill"
+
 const WorkExperience = () => {
   return (
     <section className='max-container'>
