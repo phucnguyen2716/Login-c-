4b886c556df17cac860d548252756af17e440082
Nguyen Hoang Phuc <phucnguyen2716@gmail.com>
2025-07-28 21:45:47 +0700

    Them

--- src/components/pages/QRBanking.jsx
+++ src/components/pages/QRBanking.jsx
@@ -6,7 +6,7 @@ export default function QRBanking() {
   const accountNumber = "1800773748";
   const accountName = "Nguyễn Hoàng Phúc";
   const bankName = "Ngân hàng BIDV";
-  const amount = 5000;
+  const amount = 10000;
 
   const generateQRUrl = () => {
     return `https://img.vietqr.io/image/${selectedBank}-${accountNumber}-compact2.png?amount=${amount}`;
@@ -15,7 +15,7 @@ export default function QRBanking() {
   return (
 <div className="flex justify-center pb-8" style={{ paddingTop: "25px" }}>
       <div className="bg-white border border-gray-200 rounded-xl shadow-md px-6 py-8 w-[280px] h-[300px] text-center mx-auto">
-        <h2 className="text-sm mb-10">
+        <h2 className="text-sm mb-10 text-center">
           <span className="blue-gradient_text drop-shadow strong-title">
             Đóng góp để dự án tiến xa hơn
           </span>
