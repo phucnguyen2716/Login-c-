2c624f9b029b898a828c6cfacaad509e61afb3e1
Nguyen Hoang Phuc <phucnguyen2716@gmail.com>
2025-08-10 11:37:44 +0700

    Them calendar

---
+++ src/calendar/events.jsx
@@ -0,0 +1,10 @@
+const events = [
+  {
+    id: 1,
+    title: 'Coffee with John',
+    start: '2023-12-04 10:05',
+    end: '2023-12-04 10:35',
+  },
+];
+
+export default events;
--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { ScheduleXCalendar, useCalendarApp } from "@schedule-x/react";
+import { createViewWeek, createViewMonthGrid } from "@schedule-x/calendar";
+import "@schedule-x/theme-default/dist/calendar.css";
+import './Service.css';
+import events from "../../calendar/events";
+import { createDragAndDropPlugin } from "@schedule-x/drag-and-drop";
+import { createEventModalPlugin } from "@schedule-x/event-modal"; // ⬅ thêm dòng này
 
 export default function Services() {
-  return null; // hoặc: <></>
+  const calendar = useCalendarApp({
+    views: [
+      createViewWeek(),
+      createViewMonthGrid()
+    ],
+    events: events,
+    selectedDate: '2023-12-04',
+    plugins:[
+      createEventModalPlugin(),
+      createDragAndDropPlugin(),
+    ]
+  });
+
+  return (
+    <div className="calendar-container">
+      <ScheduleXCalendar calendarApp={calendar} />
+    </div>
+  );
 }
